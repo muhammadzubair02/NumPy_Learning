2c818f483ae63b19efc348d44e81057a8fc3aa1c
Muhammad Zubair <muhammadzubairmalik118@gmail.com>
2026-05-14 22:31:29 +0500

    Create a one deminsional array

--- NumPy_Learning.py
+++ NumPy_Learning.py
@@ -1,4 +1,7 @@
 #----------NumPy Learnig Concept------------
 # First of all we import numpy as np. np is standard. 
-
 import numpy as np
+
+# Now we create a array and print the array
+arr = np.array([1,2,3,4,5,6,7,8,9])
+print(arr)
