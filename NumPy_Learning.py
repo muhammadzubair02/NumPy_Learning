6e802d8a4a6529bda4c1d89a4bda137c30b26a93
Muhammad Zubair <muhammadzubairmalik118@gmail.com>
2026-05-16 06:50:02 +0500

    Create ones array

--- NumPy_Learning.py
+++ NumPy_Learning.py
@@ -16,5 +16,9 @@ import numpy as np
 
 
 #Now we create an array by telling only number of rows and colimns and fill with some integer
-arr = np.full((3,3),5) #This line create a array with 3 rows and 3 columns and fill all the elements with 5 and store the array in a veriable name arr.
-print(arr)
+# arr = np.full((3,3),5) #This line create a array with 3 rows and 3 columns and fill all the elements with 5 and store the array in a veriable name arr.
+# print(arr)
+
+#Now we create a array whos all elements are one name ones array
+ones_arr = np.ones((3,3))
+print(ones_arr)
