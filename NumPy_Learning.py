ce7fedc382a1faa6aca0db273daddf5580eb0668
Muhammad Zubair <muhammadzubairmalik118@gmail.com>
2026-05-16 06:40:06 +0500

    Create a 2d 3d array

--- NumPy_Learning.py
+++ NumPy_Learning.py
@@ -3,5 +3,16 @@
 import numpy as np
 
 # Now we create a array and print the array
-arr = np.array([1,2,3,4,5,6,7,8,9])
-print(arr)
+# arr = np.array([1,2,3,4,5,6,7,8,9])
+# print(arr)
+
+#Now we create 2-d, 3d array and take a print
+
+# new_arr = np.array([[1,2,3,4], [5,6,7,8]])
+# print(new_arr)
+
+# new_3d_arr = np.array([[1,2,3],[4,5,6],[7,8,9]])
+# print(new_3d_arr)
+
+
+#Now we create an array by telling only number of rows and colimns and fill with some integer
